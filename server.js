530cd2ba896698c68d711c87ade3ec817a2a63e8
Golovin Maksim Dmitrievich <maks.golovin00@mail.ru>
2025-02-20 23:00:39 +0300

    Добавлены основные элементы сайта

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '\\hello.html');
+    console.log('Главная страница');
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '\\404.html');
+    console.log('Ошибка 404');
+});
+
+app.listen(5000, () => {
+    console.log('Сервер работает http://localhost:5000');
+});
